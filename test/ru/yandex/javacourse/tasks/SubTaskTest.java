8dcda11d4df96cf9d44a1ec3dc68dbd172cfc230
Наталья <natashka131097@yandex.ru>
2025-09-21 16:34:17 +0300

    v1.2.6 review_1

--- test/ru/yandex/javacourse/tasks/SubTaskTest.java
+++ test/ru/yandex/javacourse/tasks/SubTaskTest.java
@@ -1,26 +1,44 @@
 package ru.yandex.javacourse.tasks;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Тестовый класс для проверки методов класса SubTask")
 class SubTaskTest extends Task {
     private SubTask subTask;
+    private final String titleOfSubTask = "SubTask";
+    private final String descriptionOfSubTask = "Test description";
+    private final int epicId = 1;
+
 
     @BeforeEach
     void beforeEach() {
-        subTask = new SubTask("SubTask", "Test description", 1);
+        //given
+        subTask = new SubTask(titleOfSubTask, descriptionOfSubTask, epicId);
     }
 
     @Test
-    void getEpicId() {
-        assertEquals(1, subTask.getEpicId());
+    @DisplayName("Получение идентификатора эпика из подзадачи")
+    void shouldReturnCorrectEpicIdFromSubTask() {
+        //when
+        int actualEpicId = subTask.getEpicId();
+        //then
+        assertEquals(epicId, actualEpicId, "Идентификатор эпика должен совпадать с ожидаемым");
     }
 
+
     @Test
-    void setEpicId() {
-        subTask.setEpicId(2);
-        assertEquals(2, subTask.getEpicId());
+    @DisplayName("Установка идентификатора эпика должна корректно обновлять значение")
+    void shouldSetEpicIdCorrectly() {
+        //given
+        int expectedEpicId = 2;
+        //when
+        subTask.setEpicId(expectedEpicId);
+        //then
+        assertEquals(expectedEpicId, subTask.getEpicId(), "Идентификатор эпика должен быть обновлен корректно");
     }
+
 }
