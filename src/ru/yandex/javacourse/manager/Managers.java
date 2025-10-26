2f1bec4e44828d27544bdf13d181b4d9c0950206
Наталья <natashka131097@yandex.ru>
2025-10-26 08:13:59 +0300

    v1.3.1 review_2 fix returned class for Managers.getDefault() to FileBackedTaskManager

--- src/ru/yandex/javacourse/manager/Managers.java
+++ src/ru/yandex/javacourse/manager/Managers.java
@@ -1,12 +1,20 @@
 package ru.yandex.javacourse.manager;
 
 
+import ru.yandex.javacourse.exception.ManagerSaveException;
 import ru.yandex.javacourse.history.HistoryManager;
 import ru.yandex.javacourse.history.InMemoryHistoryManager;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Managers {
     public static TaskManager getDefault() {
-        return new InMemoryTaskManager();
+        try {
+            return new FileBackedTaskManager(File.createTempFile("tasks", ".csv"));
+        } catch (IOException e) {
+            throw new ManagerSaveException("Не удалось создать временный файл для FileBackedTaskManager");
+        }
     }
 
     public static HistoryManager getDefaultHistory() {
